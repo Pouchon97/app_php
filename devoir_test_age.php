113daf0ae5057717bed274b5c9583a0b2d476eaa
Pouchon97 <59709254+Pouchon97@users.noreply.github.com>
2020-03-04 11:32:43 -0800

    Update devoir_test_age.php

--- devoir_test_age.php
+++ devoir_test_age.php
@@ -1,18 +1,17 @@
 <?php
+$age_personne=Readline("Entrez votre age :");
 
-$age_personne=Readline(" Bienvenue dans notre petit programme d'autorisation pour le voyage. Entrez votre age:");
-if($age_personne>=3 and  $age_personne<=12)
-{
-    echo("$age_personne ans oh non, désolé car vous être un enfant");
+switch($age_personne){
+    case $age_personne >=3 && $age_personne<=12 :
+        echo("Enfant");
+     break;
+    case $age_personne>=13 and $age_personne<=17 :
+        echo("Mineur");
+        break;
+    case $age_personne>=18 and $age_personne<=54 :
+        echo("Majeur");
+        break;
+     case $age_personne>=55 :
+        echo("Agée");
 }
-elseif($age_personne>=13 and $age_personne<=17){
-    echo("$age_personne ans désolé car Vous être  mineur");
-}
-elseif($age_personne>=18 and $age_personne<=54){
-    echo(" $age_personne ans ah oui, Vous être  majeur");
-}
-elseif($age_personne>=55){
-    echo(" $age_personne ans ,là vous être agée");
-}
-
 ?>
