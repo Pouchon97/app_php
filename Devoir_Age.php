5d897bbd24bcc1f0c2a4b8d4def0376d8159b79b
Pouchon97 <59709254+Pouchon97@users.noreply.github.com>
2020-03-04 13:44:03 -0800

    Add files via upload

---
+++ Devoir_Age.php
@@ -0,0 +1,17 @@
+<?php
+$age_personne=Readline("Entrez votre age :");
+
+switch($age_personne){
+    case $age_personne >=3 && $age_personne<=12 :
+        echo("Enfant");
+     break;
+    case $age_personne>=13 and $age_personne<=17 :
+        echo("Mineur");
+        break;
+    case $age_personne>=18 and $age_personne<=54 :
+        echo("Majeur");
+        break;
+     case $age_personne>=55 :
+        echo("Agée");
+}
+?>
